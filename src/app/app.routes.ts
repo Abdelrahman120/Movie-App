3369f7399e0182f8cf9cd291c1ab4af4b4e077f3
“MHJaafar9869” <mostafajaafar9869@gmail.com>
2024-09-11 01:39:44 +0300

    creating main page and displaying data from api accordingly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,21 @@
 import { Routes } from '@angular/router';
 import { CardDetailsComponent } from './card-details/card-details.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 export const routes: Routes = [
   {
-    path: 'moviedetails/:id',
+    path: '',
+    component: MovieListComponent,
+  },
+  {
+    path: 'movie-details/:id',
     component: CardDetailsComponent,
     title: 'Movie Details',
   },
   {
-    path: '',
-    component: MovieListComponent
-  },
-
+    path: 'search-results/:title',
+    component: SearchResultsComponent,
+    title: "Search result",
+  }
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  faHeart = faHeart;
 }
