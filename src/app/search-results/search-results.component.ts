d2cfbf64db52b548543bd28c0af3559f6db099c4
Moataz1121 <moatazibrahim04@gmail.com>
2024-09-11 22:41:33 +0300

    show search result

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -3,24 +3,28 @@ import { ServiceRequestService } from '../services/service-request.service';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { ActivatedRoute } from '@angular/router';
 import { MovieInterface } from '../interfaces/movie-interface';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, MovieCardComponent],
   templateUrl: './search-results.component.html',
-  styleUrl: './search-results.component.css'
+  styleUrl: './search-results.component.css',
 })
 export class SearchResultsComponent {
-  constructor(private ServiceRequestService: ServiceRequestService, private ActivatedRoute: ActivatedRoute) { }
+  constructor(
+    private ServiceRequestService: ServiceRequestService,
+    private ActivatedRoute: ActivatedRoute
+  ) {}
   searchText!: string;
-  searchResult: MovieInterface[]=[];
-  title = ''
+  searchResult: MovieInterface[] = [];
+  title = '';
   ngOnInit(): void {
-    this.title = this.ActivatedRoute.snapshot.params["name"];
+    this.title = this.ActivatedRoute.snapshot.params['name'];
     console.log(this.title);
     this.ServiceRequestService.searchResult(this.title).subscribe((res) => {
-
-    })
+      this.searchResult = res.results;
+    });
   }
 }
