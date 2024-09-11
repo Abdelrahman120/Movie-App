e54bc005022e6a9405952a1cf4ff8de5abeeb365
“hamo4651” <hamo4651@gmail.com>
2024-09-11 15:36:10 +0300

    card-details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { MovieListComponent } from './movie-list/movie-list.component';
 import { CardDetailsComponent } from "./card-details/card-details.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { SearchBarComponent } from "./search-bar/search-bar.component";
+import { RecommendationsComponent } from "./recommendations/recommendations.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MovieListComponent, CardDetailsComponent, NavbarComponent, SearchBarComponent],
+  imports: [RouterOutlet, MovieListComponent, CardDetailsComponent, NavbarComponent, SearchBarComponent, RecommendationsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
-
+import { NgFor } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { ServiceRequestService } from '../services/service-request.service';
+import { ActivatedRoute, Route } from '@angular/router';
+import { DatePipe, DecimalPipe } from '@angular/common';
+ declare var $: any;
+// import * as $ from 'jquery';  // Import jQuery
 @Component({
   selector: 'app-recommendations',
   standalone: true,
-  imports: [],
+  imports: [NgFor,FontAwesomeModule,DatePipe, DecimalPipe],
   templateUrl: './recommendations.component.html',
   styleUrl: './recommendations.component.css'
 })
 export class RecommendationsComponent {
+ 
+  @Input() id: any='';
+  movieRecommendations: any
+  constructor(private ServiceRequestService:ServiceRequestService ,private route:ActivatedRoute){}
 
+
+  faHeart=faHeart ;
+
+
+  ngOnInit() {
+    
+     this.id = this.route.snapshot.paramMap.get('id') || '';
+    if (this.id) {
+      this.ServiceRequestService.getRecommendations(this.id).subscribe((res: any) => {
+        console.log("Data fetched from service:", res);
+        this.movieRecommendations = res.results;
+        console.log( this.movieRecommendations);
+
+      }, error => {
+        console.error("Error fetching movie recommendations", error);
+      });
+    }
+    console.log( this.movieRecommendations);
+
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+    $('.carousel').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 900, 
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        }
+,
+{
+  breakpoint: 768, 
+  settings: {
+    slidesToShow: 3,
+    slidesToScroll: 1
+  }
+}     
+,
+{
+  breakpoint: 600, 
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 1
+  }
+}    ]
+    });
+  }, 1000);
+  }
 }
