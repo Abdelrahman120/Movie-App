9054f9b602a368e7cd58560c92f8c0499e1c3671
Moataz1121 <moatazibrahim04@gmail.com>
2024-09-12 23:33:55 +0300

    final search & show the length of cart

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +12,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private cartService: CartService){}
   faHeart = faHeart;
+
+  get cartLength(): number {
+    return this.cartService.getCartItems().length;
+  }
+
 }
