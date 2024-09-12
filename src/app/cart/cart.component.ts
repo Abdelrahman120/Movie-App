82cf0f71272dacc528d27ffecf4ccb7856c848aa
unknown <af348565@gmail.com>
2024-09-12 23:13:11 +0300

    deign cart watch list

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { CartService } from '../services/cart.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, NgClass],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 cartItems: any[] = [];
+faStar = faStar;
+  faHeart = faHeart;
 constructor(private cartService: CartService) { }
 
 ngOnInit(): void {
