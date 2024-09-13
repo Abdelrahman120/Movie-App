e5bae65bfce0db29ca04b02a1a5769e03d820357
unknown <af348565@gmail.com>
2024-09-13 15:25:00 +0300

    create not-found & enhance in cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
-import { NgClass } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [FontAwesomeModule, NgClass],
+  imports: [FontAwesomeModule, NgClass, DatePipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
@@ -15,7 +16,7 @@ export class CartComponent {
 cartItems: any[] = [];
 faStar = faStar;
   faHeart = faHeart;
-constructor(private cartService: CartService) { }
+constructor(private cartService: CartService, private router: Router) { }
 
 ngOnInit(): void {
 
@@ -32,5 +33,8 @@ clearCart() {
 removeFromCart(movie: any) {
   this.cartService.removeFromCart(movie);
   this.cartItems = this.cartItems.filter((item) => item.id !== movie.id);
+}
+goBack() {
+  this.router.navigate(['/']);
 }
 }
