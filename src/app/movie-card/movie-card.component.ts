f4e42dd6a31adf954c7585bfb4c74d7d0eba311e
Moataz1121 <moatazibrahim04@gmail.com>
2024-09-14 20:27:48 +0300

    handel heart color & not found page

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ServiceRequestService } from '../services/service-request.service';
 import { Router } from '@angular/router';
-import { DatePipe, DecimalPipe } from '@angular/common';
+import { DatePipe, DecimalPipe, NgClass } from '@angular/common';
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CartService } from '../services/cart.service';
@@ -9,11 +9,12 @@ import { CartService } from '../services/cart.service';
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [DatePipe, FontAwesomeModule, DecimalPipe],
+  imports: [DatePipe, FontAwesomeModule, DecimalPipe , NgClass],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.css'
 })
 export class MovieCardComponent {
+  isInWatchlist: boolean = false;
   @Input() movie: any;
   faHeart = faHeart;
   constructor(private cartService: CartService ,private ServiceRequestService: ServiceRequestService, private router: Router) { }
@@ -23,9 +24,14 @@ export class MovieCardComponent {
   }
   addToCart(movie: any) {
     this.cartService.addToCart(movie);
+    this.isInWatchlist = true;
   }
-
+  isMovieInWatchlist(movieId: string): boolean {
+    const cartItems = this.cartService.getCartItems();
+    return cartItems.some(item => item.id === movieId);
+  }
   ngOnInit(): void {
+    this.isInWatchlist = this.isMovieInWatchlist(this.movie.id);
     // console.log(this.movie);
   }
 }
