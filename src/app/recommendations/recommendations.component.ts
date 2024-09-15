3cfe2b9693aea4b84a4cf17c5d9151d37c1cb459
Moataz1121 <moatazibrahim04@gmail.com>
2024-09-15 14:25:25 +0300

    handel recommendation

--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -1,58 +1,63 @@
 import { NgFor } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { ServiceRequestService } from '../services/service-request.service';
-import { ActivatedRoute, Route, Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { RecommendationInterface } from '../interfaces/recommendation-interface';
 import { CartService } from '../services/cart.service';
 declare var $: any;
+
 @Component({
   selector: 'app-recommendations',
   standalone: true,
-  imports: [NgFor, FontAwesomeModule, DatePipe, DecimalPipe,RouterLink],
+  imports: [NgFor, FontAwesomeModule, DatePipe, DecimalPipe, RouterLink],
   templateUrl: './recommendations.component.html',
-  styleUrl: './recommendations.component.css',
+  styleUrls: ['./recommendations.component.css'],
 })
-export class RecommendationsComponent {
+export class RecommendationsComponent implements OnInit {
   @Input() id: any = '';
   movieRecommendations: RecommendationInterface[] = [];
-  constructor(
-    private ServiceRequestService: ServiceRequestService,
-    private route: ActivatedRoute,private cartService: CartService
-    , private router: Router
-  ) {}
-
   faHeart = faHeart;
 
+  constructor(
+    private serviceRequestService: ServiceRequestService,
+    private route: ActivatedRoute,
+    private cartService: CartService,
+    private router: Router
+  ) {}
+
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id') || '';
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id') || '';
+      this.fetchRecommendations(); 
+    });
+  }
+
+  fetchRecommendations() {
     if (this.id) {
-      this.ServiceRequestService.getRecommendations(this.id).subscribe(
+      this.serviceRequestService.getRecommendations(this.id).subscribe(
         (res: any) => {
-          console.log('Data fetched from service:', res);
           this.movieRecommendations = res.results;
-          console.log(this.movieRecommendations);
         },
-        (error) => {
-          console.error('Error fetching movie recommendations', error);
-        }
+        
       );
     }
-    console.log(this.movieRecommendations);
   }
- 
+
   addToCart(movie: any) {
     this.cartService.addToCart(movie);
   }
+
   goToDetails(id: number) {
-    this.router.navigate(['/movie-details', Number(id) ]);
+    this.router.navigate(['/movie-details', Number(id)]);
   }
+
   ngAfterViewInit() {
     setTimeout(() => {
       $('.carousel').slick({
-        slidesToShow:5,
+        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
